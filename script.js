c3ac587f71140ac2a75be3379d5952eda2da10ee
OliveiraLucas5248 <110700571+OliveiraLucas5248@users.noreply.github.com>
2026-02-02 18:43:14 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+/*const btn = document.querySelector('.menu-toggle');
+const nav = document.querySelector('.nav-menu');
+
+btn.addEventListener('click', () => {
+  nav.classList.toggle('active');
+});
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    const target = document.querySelector(this.getAttribute('href'));
+    if (target) {
+      target.scrollIntoView({ behavior: 'smooth' });
+    }
+
+    nav.classList.remove('active');
+  });
+});
+*/
